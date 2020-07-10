d2e60e6adc97f8cec4711eaf8f7bd229d0a31ff4
65814829 <jchickmm2@gmail.com>
2020-07-10 18:38:17 -0400

    edit

--- commands/test.js
+++ commands/test.js
@@ -4,8 +4,13 @@ module.exports = {
   name: "test",
   ownerOnly: true,
   async execute(message, args) {
-
-    message.channel.send('lol')
+const msg = message.channel.messages.fetch("731265534301044826")
+msg.embeds[0].setFooter(
+  "Please refrain from chatting in this channel so notes don't get drowned")
+    msg.edit(
+      new Discord.MessageEmbed(msg.embeds[0])
+    );
+    console.log('done')
 
   }
 };
