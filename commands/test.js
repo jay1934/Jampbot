075718b8f94a5ec6cfced2f2c47c3cd603214a06
65814829 <jchickmm2@gmail.com>
2020-07-10 18:56:03 -0400

    edit

--- commands/test.js
+++ commands/test.js
@@ -4,12 +4,17 @@ module.exports = {
   name: "test",
   ownerOnly: true,
   async execute(message, args) {
-const msg = message.channel.messages.fetch("731265534301044826")
-msg.embeds[0].setFooter(
-  "Please refrain from chatting in this channel so notes don't get drowned")
-    msg.edit(
-      new Discord.MessageEmbed(msg.embeds[0])
-    );
+    const msg = message.channel.messages.fetch("731265534301044826")
+    const newE = new Discord.MessageEmbed()
+      .setTitle('Welcome!')
+      .setDescription('This channel can be used as a to-do list (for example, a note could read ``discuss rerating _____ level``), or as an easy way to remember things.\n\nTo add a note, simply use ``!note message`` in any channel (preferably not this one). Unresolved notes will be pinned.')
+      .setFooter("Please refrain from chatting in this channel so notes don't get drowned")
+      message.channel.messages.fetch({around: "731265534301044826", limit: 1})
+    .then(msg => {
+        const fetchedMsg = msg.first();
+        fetchedMsg.edit(newE);
+    });
+    message.delete()
     console.log('done')
 
   }
