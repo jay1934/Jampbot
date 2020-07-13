11f42c8b32ebe1fc56c3124464cfa7fee69129a5
65814829 <jchickmm2@gmail.com>
2020-07-13 11:17:11 -0400

    dont -> don't

--- commands/judges.js
+++ commands/judges.js
@@ -4,7 +4,7 @@ module.exports = {
   judgeOnly: true,
   async execute(message, args) {
     message.channel.send(
-      'Please be patient and realize that the judge team are real people with real lives that take precedent over getting your level approved. This is not our job, it is a hobby. Please dont make it feel like more work than it should.\n\nWe try to get 3 votes on each submission, which can take some time, especially on the more difficult levels.\n\nThank You,\nThe Jamp Judge team'
+      "Please be patient and realize that the judge team are real people with real lives that take precedent over getting your level approved. This is not our job, it is a hobby. Please don't make it feel like more work than it should.\n\nWe try to get 3 votes on each submission, which can take some time, especially on the more difficult levels.\n\nThank You,\nThe Jamp Judge team"
     );
   },
 };
