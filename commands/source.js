7142f0b8b0407169868c8ded22e75cb97115bdfa
65814829 <jchickmm2@gmail.com>
2020-07-20 15:25:16 -0400

    added source cmd because why not

---
+++ commands/source.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'source',
+  async execute(message, args) {
+    message.channel.send(
+      'Jampbot Deluxe source code: https://github.com/TeamShellSMM/ShellBot3000\nJampbot++ source code: https://github.com/Lioness100/Jampbot'
+    );
+  },
+};
