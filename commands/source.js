899ac60feda0fd3d44c4e043fc2e27b0d05308d1
65814829 <jchickmm2@gmail.com>
2020-07-20 15:31:34 -0400

    ignored prettier config file and grammar-ized source

--- commands/source.js
+++ commands/source.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'source',
   async execute(message, args) {
     message.channel.send(
-      'Jampbot Deluxe source code: https://github.com/TeamShellSMM/ShellBot3000\nJampbot++ source code: https://github.com/Lioness100/Jampbot'
+      'Jampbot Deluxe source code: <https://github.com/TeamShellSMM/ShellBot3000>\nJampbot++ source code: <https://github.com/Lioness100/Jampbot>'
     );
   },
 };
