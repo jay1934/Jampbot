a69ce1b1cd0f9d63f519585538c2f6d30eb92048
65814829 <jchickmm2@gmail.com>
2020-07-24 11:41:54 -0400

    Added dikdik per TIB's request idk why I do this

---
+++ commands/dikdik.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const { getRandomArrElement } = require('../utils/functions');
+
+const dikdik = [
+  'https://cdn.discordapp.com/attachments/699612856018272289/735301972420329530/266px-Dik-dik.png',
+  'https://cdn.discordapp.com/attachments/699612856018272289/735302976356483132/Z.png',
+  'https://cdn.discordapp.com/attachments/699612856018272289/735903024769138778/oNYchXMEZRmirb0-tn5wnrsl6i-_iCey2qJBnjrmQxI.png',
+  'https://cdn.discordapp.com/attachments/699612856018272289/735902893093421116/UcwBFGiJADcdj3EEojjU_4wKD5xR5R4oGDxJ05ktgSs.png',
+];
+module.exports = {
+  name: 'dikdik',
+  blacklist: true,
+  async execute(message, args) {
+    if (
+      message.author.id === '592950968354865162' ||
+      message.author.id === '591923920261873677'
+    ) {
+      const lol = new Discord.MessageEmbed().setImage(
+        getRandomArrElement(dikdik)
+      );
+      message.channel.send(lol);
+    }
+  },
+};
