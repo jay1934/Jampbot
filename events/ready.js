ff7eb14f80db22c0bf3f7fd37f1eae54510b599b
65814829 <jchickmm2@gmail.com>
2020-08-04 19:59:53 -0400

    🎉 Added XP and level system

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Levels = require('discord-xp');
 const { getChannel, toFirstUpperCase } = require('../utils/functions');
 
 module.exports = async (client) => {
@@ -19,6 +20,9 @@ module.exports = async (client) => {
       .join('\n')
   );
   client.mongoose = require('../utils/mongoose');
+  await Levels.setURL(process.env.MONGO)
+    .then(() => console.log('EXP Module Connected'))
+    .catch((err) => console.log(err));
   await client.mongoose.init();
   console.log("Everything's Connected!");
 };
