cbafd41bc8d9e208e01a75b056eb2b4771a2c7b7
65814829 <jchickmm2@gmail.com>
2020-08-04 13:46:37 -0400

    Condensed

--- bot.js
+++ bot.js
@@ -1,35 +1,23 @@
-// teaching the bot to require all needed packages
-
-// this package let's us connect to Discord, and allows us to use various Discord related plugins
 const Discord = require('discord.js');
 
-// these packages are just helpful addons for commands, nothing important
-
-const fs = require('fs'); // file save
+const fs = require('fs');
 require('dotenv').config();
 
-// creating a new discord client (the bot)
 const client = new Discord.Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 
 require('./utils/eventLoader.js')(client);
 
-// creating a collection for our bot commands
 client.commands = new Discord.Collection();
 
-// reads files to see if they are in the commands folder and end in .js (reads all bot commands), then identifies them as such
-
 const commandFiles = fs
-  .readdirSync('./commands') // read all files in the commands folder
-  .filter((file) => file.endsWith('.js')); // only consider .js files
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-
-  // set a new item in the Collection with the key as the command name and the value as the exported module (file)
   client.commands.set(command.name, command);
 }
 
-// this allows the bot to login with token
 client.login(process.env.TOKEN);
