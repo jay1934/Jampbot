002abb203f4b6f026a7ff09ba6b4250d1fe1b9f9
65814829 <jchickmm2@gmail.com>
2020-08-05 09:12:01 -0400

    added more info

--- commands/exp/xpinfo.js
+++ commands/exp/xpinfo.js
@@ -1,11 +1,12 @@
 module.exports = {
   name: 'xpinfo',
+  aliases: ['expinfo'],
   category: 'EXP',
   usage: '!xpinfo',
   description: 'Displays XP information',
   async execute(message, args) {
     message.channel.send(
-      'You can earn XP by participating in conversations! You will get 1-30 XP every message sent (some channels are excluded). **Server Boosters get a 2x EXP multiplier!**\n\nTry ``!level`` and ``!leaderboard``!'
+      'You can earn XP by participating in conversations! You will get 1-30 XP points every message sent (some channels are excluded) (max **once** per minute). **Server Boosters get a 2x EXP multiplier!**\n\nTry ``!level`` and ``!leaderboard``!'
     );
   },
 };
