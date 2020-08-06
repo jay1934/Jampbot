f6e1e9aba4c27cf04b60ef0e55a10d073ecad363
65814829 <jchickmm2@gmail.com>
2020-08-06 17:06:44 -0400

    Debugged

--- commands/exp/promote.js
+++ commands/exp/promote.js
@@ -8,12 +8,12 @@ module.exports = {
   async execute(message, args) {
     const user = message.mentions.users.first();
     if (!user) return message.channel.send("You didn't mention a user!");
-    const xp = parseFloat(args[1]);
+    const xp = parseInt(args[1]);
     if (!xp)
       return message.channel.send(
         "You didn't specify how many EXP levels to award!"
       );
-    Levels.appendXp(user.id, message.guild.id, xp)
+    Levels.appendLevel(message.author.id, message.guild.id, xp)
       .then(() =>
         message.channel.send(
           `**${xp} Levels** have been awarded to **${
