4e56513568624a19fb53ed8c7e7d7c2327e4feff
65814829 <jchickmm2@gmail.com>
2020-08-06 17:04:38 -0400

    Coverted leaderboard from msg to embed

--- commands/exp/leaderboard.js
+++ commands/exp/leaderboard.js
@@ -1,9 +1,11 @@
 const Levels = require('discord-xp');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'leaderboard',
   category: 'EXP',
   usage: '!leaderboard',
+  aliases: ['lb'],
   blacklist: true,
   description: 'Shows guildXPleaderboard.',
   async execute(message, args) {
@@ -13,13 +15,24 @@ module.exports = {
       rawLeaderboard
     );
 
-    const lb = leaderboard.map(
+    const data = new MessageEmbed()
+      .setColor('GREEN')
+      .setTitle('EXP Leaderboard')
+      .setFooter('Use !exp to see your rank!');
+    for (const lb of leaderboard) {
+      data.addField(
+        `${lb.position}. ${lb.username}#${lb.discriminator}`,
+        `**Level:** ${lb.level}\n**EXP:** ${lb.xp.toString()}`
+      );
+    }
+    /* const lb = leaderboard.map(
       (e) =>
         `**${e.position}.** ${e.username}#${e.discriminator}\n**Level:** \`\`${
           e.level
         }\`\`\n**XP:** \`\`${e.xp.toLocaleString()}\`\``
-    );
+    ); */
 
-    message.channel.send(`**EXP Leaderboard**:\n\n${lb.join('\n\n')}`);
+    // message.channel.send(`**EXP Leaderboard**:\n\n${lb.join('\n\n')}`);
+    message.channel.send(data);
   },
 };
